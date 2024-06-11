d896be52e729f7e262762656d4adddd41d451918
Abhishek Yelley <yabhishek.19.cse@anits.edu.in>
2024-06-11 23:41:38 +0530

    upd: hidden anchor tag is used to navigate to force refresh
    probably a bad way... need to find if form actions can do something so that image reiviewer loading state is previewed on every form submit

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,107 +1,88 @@
 "use client";
-import ApiData from "@/utils/ApiData";
 import FormData from "@/utils/FormData";
 
 import { useRouter } from "next/navigation";
 import {
   ChangeEvent,
-  Dispatch,
   FormEvent,
-  SetStateAction,
   useState,
+  useRef
 } from "react";
-
-interface SearchBoxProps {
-  // fetcher: (url: string) => void,
-  apiData?: ApiData | null;
-  setApiData?: Dispatch<SetStateAction<ApiData | null>>;
-  setIsVisible?: Dispatch<SetStateAction<boolean>>;
-  isFetching?: boolean;
-  BASE_URL?: string;
-  IMAGE_URL?: string;
-  myRef?: React.MutableRefObject<HTMLElement | null>;
-  setQueryURL?: Dispatch<SetStateAction<string>>;
-}
 
 export default function SearchBox() {
   const router = useRouter();
+  const gay = useRef<any>(null);
 
+  // const [isPending, startTransition] = useTransition();
   const [formData, setFormData] = useState<FormData>({
-    blink: "",
+    url: "",
   });
-
-  // const [submitted, setSubmitted] = useState(false);
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const url = formData.blink.trim();
+    const url = formData.url.trim();
     const hasSpaces = /\s/g.test(url);
     if (hasSpaces) {
       console.log("has spaces");
-      // reject
       return;
     }
-    router.push(`/review/?url=${url}&img=1`);
-    // setSubmitted(true);
-
-    // console.log(BASE_URL + url);
-    // setQueryURL(BASE_URL + url);
-    // fetcher(BASE_URL + formData.blink);
+    setFormData({ url: url });
+    gay.current.click();
+    /*startTransition(() => {
+      router.push(`/review/?url=${url}&img=1`);
+      });*/
   };
 
   const handleClear = () => {
-    setFormData({ blink: "" });
-    // setSubmitted(false);
+    setFormData({ url: "" });
   };
 
-  // Event handler to update form data on input change
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
       ...prevData,
       [name]: value,
     }));
-    if (value.trim() === "") {
-      // setSubmitted(false);
-    }
   };
 
   return (
-    <form
-      className="flex w-full font-medium m-auto items-center"
-      onSubmit={handleSubmit}>
-      <input
-        className="w-full rounded-full rounded-r-none my-5 py-3 pl-5 
+    <>
+      <a href={`/review/?url=${formData.url}&img=1`} ref={gay} hidden></a>
+      <form
+        className="flex w-full font-medium m-auto items-center"
+        onSubmit={handleSubmit}>
+        <input
+          className="w-full rounded-full rounded-r-none my-5 py-3 pl-5 
         bg-neutral-900 active:bg-neutral-900 placeholder:text-neutral-400 
         text-neutral-100 text-base md:text-lg focus:outline-none h-fit"
-        value={formData.blink}
-        onChange={handleInputChange}
-        type="text"
-        name="blink"
-        required
-        // autoFocus
-        placeholder="Paste your link here."
-      />
-      <button
-        className={`bg-neutral-900 text-neutral-400 font-medium pr-10 pl-5
-          py-3 md:py-3.5 text-xl md:text-2xl ${
-            formData.blink === "" ? "hidden" : ""
-          }`}
-        type="button"
-        onClick={handleClear}>
-        <img
-          src="/xmark-solid.svg"
-          alt="clear"
-          className="h-6 w-6"
+          value={formData.url}
+          onChange={handleInputChange}
+          type="text"
+          name="url"
+          required
+          // autoFocus
+          placeholder="Paste your link here."
         />
-      </button>
-      <button
-        className="bg-blue-400 text-neutral-900 font-bold px-5
+        <button
+          className={`bg-neutral-900 text-neutral-400 font-medium pr-10 pl-5
+          py-3 md:py-3.5 text-xl md:text-2xl ${formData.url === "" ? "hidden" : ""
+            }`}
+          type="button"
+          onClick={handleClear}>
+          <img
+            src="/xmark-solid.svg"
+            alt="clear"
+            className="h-6 w-6"
+          />
+        </button>
+        <button
+          className="bg-blue-400 text-neutral-900 font-bold px-5
         py-3 rounded-full text-base md:text-lg  right-3 md:right-5
         relative disabled:bg-neutral-600 disabled:text-neutral-100"
-        type="submit">
-        Submit!
-      </button>
-    </form>
+          type="submit" >
+          {"Submit!"}
+        </button>
+      </form>
+    </>
   );
 }
