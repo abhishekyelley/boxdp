bc0784621122b2847d737798760f2fdbbee29ecf
Abhishek Yelley <abhishekyelley@gmail.com>
2024-11-23 13:25:13 +0530

    merge: main to mine

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,6 @@ import Header from "@/components/Header";
 // const inter = Karla({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  metadataBase: new URL('http://localhost:3000/'),
   title: "boxd-pics",
   description:
     "Get a prettier image of your letterboxd review. Just provide the URL.",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,4 @@
-import { Metadata, ResolvingMetadata } from "next";
 import Home from "@/components/Home";
-export const dynamic = "force-dynamic";
-
-type Props = {
-  params: { id: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-};
-
-export async function generateMetadata(
-  { params, searchParams }: Props,
-  parent: ResolvingMetadata
-): Promise<Metadata> {
-  // read route params
-  const id = params.id;
-
-  // optionally access and extend (rather than replace) parent metadata
-  const previousImages = (await parent).openGraph?.images || [];
-
-  return {
-    title: "Boxdp",
-    openGraph: {
-      images: ["/api/image", ...previousImages],
-    },
-  };
-}
 
 export default function Page() {
   return <Home />;
